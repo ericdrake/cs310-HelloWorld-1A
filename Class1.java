74abd5c01240f1ab29ecdcd808bbfd87fc1e657d
Eric Drake <wdrake@stu.jsu.edu>
2022-01-30 11:12:03 -0600

    Cleaned it up

--- Class1.java
+++ Class1.java
@@ -7,15 +7,15 @@ public class Class1 {
 
     private void f1() {
         // v1.0
-    }
 
-}
+    }
     private void f2() {
         // Placeholder method (v2.0)
-}
+    }
     private void f3() {
         // Placeholder method (v3.0)
-}
+    }   
     private void f4() {
         // Placeholder method (v4.0)
+    }
 }
